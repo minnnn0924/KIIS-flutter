150fc2b32bdf13f14aae5c654ef1b271b6882400
minnnn0924 <wkdtjalsrhkd@naver.com>
2025-07-16 17:04:58 +0900

    Update deployed site

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3537032255"
+    serviceWorkerVersion: "2668669013"
   }
 });
